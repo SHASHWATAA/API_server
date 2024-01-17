a51fde991d8b4bdb918c248bec76f9e2488a8154
shashwataryal <shashwat221@gmail.com>
2024-01-18 00:54:53 +1100

    removed reload true to push to prod.

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ async def movie_time():
     return "plex turned on"
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=9530, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=9530, reload=False)
