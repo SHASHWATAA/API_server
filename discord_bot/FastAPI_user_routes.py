226a69116a6205f2b9d6b712a9f15921a6b39f77
SHASHWATAA <shashwat221@gmail.com>
2024-05-06 23:24:27 +1000

    Combined discord bot into API server to send notifications

--- discord_bot/FastAPI_user_routes.py
+++ discord_bot/FastAPI_user_routes.py
@@ -1,20 +1,46 @@
 # user_routes.py
-from fastapi import APIRouter
-import credentials
-from discord_bot.discord_bot import bot
+from typing import List
+
+from fastapi import APIRouter, Query
+
+from discord_bot.discord_bot import bot, send_images
 
 router = APIRouter()
 
 
 # Define a FastAPI route that accepts a parameter
-@router.get("/discord-bot/send-message")
+@router.post("/discord-bot/send-message")
 async def send_to_channel(message: str, channel_id: int):
     # Replace "your_channel_id" with the actual channel ID where you want to send the message
     # channel_id = credentials.channel_id  # Example channel ID
     channel = bot.get_channel(channel_id)
 
     if channel:
-        await channel.send(message)
-        return {"status": "Message sent successfully!"}
+        message = await channel.send(message)
+        return {"status": f"Message sent successfully!",
+                "message_id": message.id}
     else:
         return {"error": "Channel not found."}
+
+
+@router.post("/discord-bot/send-images")
+async def send_to_channel(channel_id: int, image_paths: List[str] = Query(...)):
+    print(image_paths)
+    # Replace "your_channel_id" with the actual channel ID where you want to send the message
+    # channel_id = credentials.channel_id  # Example channel ID
+    await send_images(image_paths, channel_id)
+
+
+@router.post("/discord-bot/add-reaction")
+async def add_reaction_to_message(message_id: int, channel_id: int, reaction: str):
+    # Replace 'your_bot' with your actual bot instance
+    message = await bot.get_channel(channel_id).fetch_message(message_id)
+
+    if message:
+        try:
+            await message.add_reaction(reaction)
+            return {"status": "Reaction added successfully!"}
+        except Exception as e:
+            return {"error": f"Error adding reaction: {str(e)}"}
+    else:
+        return {"error": "Message not found."}
--- discord_bot/discord_bot.py
+++ discord_bot/discord_bot.py
@@ -1,6 +1,8 @@
 import asyncio
-
+from datetime import datetime
+from invoice_generator import pdf_generator
 import discord
+import requests
 from discord.ext import commands
 
 import credentials
@@ -22,6 +24,116 @@ async def get_dates(ctx):  # a slash coand will be created with the name "ping"
 async def on_ready():
     print(f"Logged in as {bot.user} running together with FastAPI!")
 
+
+class InvoiceGenerator:
+    selection = {
+        "schedule": {"Monday": "Canvas", "Tuesday": "Canvas", "Wednesday": "Cyrus", "Thursday": "Canvas", "Friday": "Canvas"},
+        "invoice_week": "This week" if datetime.now().isoweekday() > 4 else "Last week"
+    }
+
+    class ScheduleConfirmationButtons(discord.ui.View):
+        def __init__(self, *, timeout=None):
+            super().__init__(timeout=timeout)
+            self.add_item(discord.ui.Button(label="Okay", custom_id="okay", style=discord.ButtonStyle.green))
+            self.add_item(discord.ui.Button(label="Not Okay", custom_id="not_okay", style=discord.ButtonStyle.red))
+
+        async def interaction_check(self, interaction: discord.Interaction) -> bool:
+            if interaction.data["custom_id"] == "okay":
+                await interaction.response.send_message("Great! Generating your invoice...")
+                schedule = InvoiceGenerator.selection['schedule']
+                canvas_days = [day for day, company in schedule.items() if company == 'Canvas']
+                cyrus_days = [day for day, company in schedule.items() if company == 'Cyrus']
+                # print(canvas_days)
+                # print(cyrus_days)
+                # url = "http://127.0.0.1:8000"
+                # payload = {"canvas_days": canvas_days, "cyrus_days": cyrus_days}
+                # invoice_generate_url = "/invoice_generator/generate_invoice"
+                # print("sending_request to generate")
+                # print(requests.post(url + invoice_generate_url, params=payload))
+                pdf_generator.main(canvas_days, cyrus_days)
+                print("request to generate sent")
+                for item in self.children:
+                    item.disabled = True
+                await interaction.message.edit(view=self)
+
+                return False
+            elif interaction.data["custom_id"] == "not_okay":
+                print("Confirmation declined")
+                await interaction.message.delete()
+                await interaction.response.send_message(view=InvoiceGenerator.ScheduleButtons())
+                return False
+
+    class ScheduleButtons(discord.ui.View):
+
+        def __init__(self, *, timeout=None):
+            super().__init__(timeout=timeout)
+            self.days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+            self.companies = ["Canvas", "Cyrus"]
+            self.weeks = ["This week", "Last week"]
+
+            # Create a button for each company for each day
+            for day_index, day in enumerate(self.days):
+                self.add_item(discord.ui.Button(label=day, disabled=True, row=day_index))
+
+                for company_index, company in enumerate(self.companies):
+                    style = discord.ButtonStyle.primary if InvoiceGenerator.selection["schedule"][
+                                                               day] == company else discord.ButtonStyle.red
+                    self.add_item(
+                        discord.ui.Button(label=f"{company}", custom_id=f"{day}_{company}", style=style, row=day_index))
+
+            for week_index, week in enumerate(self.weeks):
+                style = discord.ButtonStyle.primary if InvoiceGenerator.selection["invoice_week"] == week else discord.ButtonStyle.red
+                row = 0 if week == "This week" else 1
+                self.add_item(discord.ui.Button(label=f"{week}", custom_id=f"week_{week}", style=style, row=row))
+
+            # Add a "Done" button
+            self.add_item(discord.ui.Button(label="Done", custom_id="done", style=discord.ButtonStyle.green, row=4))
+
+        async def interaction_check(self, interaction: discord.Interaction):
+            if interaction.data["custom_id"] == "done":
+                schedule_selections = "\n".join(
+                    f"{day}: {company}" for day, company in InvoiceGenerator.selection["schedule"].items() if company is not None)
+                invoice_week = InvoiceGenerator.selection["invoice_week"]
+                await interaction.response.send_message(
+                    f"Schedule confirmed!\n{schedule_selections}\nInvoice Week: {invoice_week}", view=InvoiceGenerator.ScheduleConfirmationButtons())
+                for item in self.children:
+                    item.disabled = True
+                await interaction.message.edit(view=self)
+            elif "week" in interaction.data["custom_id"]:
+                _, week = interaction.data["custom_id"].split('_')
+                InvoiceGenerator.selection["invoice_week"] = week
+                await interaction.response.defer()
+                await interaction.message.edit(view=InvoiceGenerator.ScheduleButtons())
+            else:
+                day, company = interaction.data["custom_id"].split('_')
+                InvoiceGenerator.selection["schedule"][day] = company
+                await interaction.response.defer()
+                await interaction.message.edit(view=InvoiceGenerator.ScheduleButtons())
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    reaction_channel_id = reaction.message.channel.id
+
+    if user == bot.user:
+        return
+
+    if reaction_channel_id == 1187345577235730466:
+        if str(reaction.emoji) == "👍":
+            await reaction.message.channel.send("Select two companies for each weekday:", view=InvoiceGenerator.ScheduleButtons())
+
+
+async def send_images(image_paths: list, channel_id: int):
+    channel = bot.get_channel(channel_id)
+
+    files = []
+    for image_path in image_paths:
+        filename = f"{image_path.split('/')[-1]}"  # Extract the filename from the path
+        file = discord.File(image_path, filename=filename)
+        files.append(file)
+    await channel.send(files=files)
+
+
 async def run():
     try:
         await bot.start(credentials.bot_token)  # Replace "token" with your actual bot token
@@ -30,8 +142,8 @@ async def run():
 
 
 # Run the bot
+# asyncio.run(run())
 asyncio.create_task(run())
-
 
 if __name__ == '__main__':
     bot.run(credentials.bot_token)
